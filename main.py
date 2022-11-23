e6c48ba9027ea7ea143a3baa22409e56692704db
JarodEphec <j.boulangerflemal@students.ephec.be>
2022-11-23 16:48:15 +0100

    Compare la similarité de deux signaux

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import wave
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
+from scipy import fft
 
 
 def show_plot(file_name):
@@ -45,17 +46,40 @@ def compare_two_signal(signal1, signal2):
             result += 1
     return result / len(audio2_as_np_int16) * 100
 
+def compute_similarity(ref_rec,input_rec):
+    ## Time domain similarity
+    ref_time = np.correlate(ref_rec, ref_rec)
+    inp_time = np.correlate(ref_rec, input_rec)
+    diff_time = abs(ref_time-inp_time)
+
+    ## Freq domain similarity
+    ref_freq = np.correlate(np.fft.fft(ref_rec), np.fft.fft(ref_rec))
+    inp_freq = np.correlate(np.fft.fft(ref_rec), np.fft.fft(input_rec))
+    diff_freq = abs(ref_freq-inp_freq)
+
+    ## Power similarity
+    ref_power = np.sum(ref_rec**2)
+    inp_power = np.sum(input_rec**2)
+    diff_power = abs(ref_power-inp_power)
+
+    return float(diff_time), float(diff_freq), float(diff_power)
+
 
 if __name__ == '__main__':
     print("similitude de ", compare_two_signal(
-        'bonjour.wav', 'bonjour2.wav'), ' %')
+        'bonjour.wav', 'gab_bonjour_1.wav'), ' %')
     plt.subplot(3, 1, 1)
     show_plot('bonjour.wav')
     plt.subplot(3, 1, 2)
-    show_plot('bonjour.wav')
+    show_plot('patate.wav')
     plt.subplot(3, 1, 3)
     raw = np.correlate(file_to_process(
-        'bonjour.wav'), file_to_process('bonjour2.wav'), 'same')
+        'bonjour.wav'), file_to_process('patate.wav'), 'same')
     print("Cross correlation : ", np.average(raw))
     show_plot_raw(raw)
     plt.show()
+    corr = fft.ifft(fft.fft(file_to_process('bonjour.wav')) * np.conj(fft.fft(file_to_process('patate.wav'))))
+    print(np.average(corr))
+    print(compute_similarity(file_to_process('bonjour.wav'), file_to_process('bonjour.wav')))
+    print(compute_similarity(file_to_process('bonjour.wav'), file_to_process('patate.wav')))
+    print(compute_similarity(file_to_process('bonjour.wav'), file_to_process('bonjour2.wav')))
